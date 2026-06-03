959e2d7f546e548790a4ca271f3954aa9bae1371
jonathanlukehames-tech <jonathanluke.hames@gmail.com>
2026-06-03 20:59:48 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -128,7 +129,7 @@ app.get("/oauth/authorize", (req, res) => {
 </html>`);
 });
 
-app.use(express.urlencoded({ extended: true }));
+
 
 app.post("/oauth/login", (req, res) => {
   const { email, password, redirect_uri, state } = req.body;
